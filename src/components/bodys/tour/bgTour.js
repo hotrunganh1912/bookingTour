713db9100427c3450e485f23deaba9b1dedd64c6
anh <hotrunganhht1912@gmail.com>
2020-04-21 22:18:41 +0700

    phan trang  ok

--- src/components/bodys/tour/bgTour.js
+++ src/components/bodys/tour/bgTour.js
@@ -8,15 +8,20 @@ import { removeAccents } from "../../../common/funcCommon";
 import Waiting from "../../../common/waiting";
 import NotFindData from "../../../common/notFindata";
 import CheckConnect from "../../../common/checkConnect";
+import MyPagination from "../../../common/my-pagination";
 
 const BgTour = (props) => {
   console.log("props :", props);
+  const _limit = 6;
+  const [currentPage, setCurrentPage] = useState(1);
 
   const q = props.data.search.q;
   const typeTour = props.data.search.typeTour;
   const dateStart = props.data.search.dateStart;
 
   const [countFail, setCountFail] = useState(0);
+
+  const [indexDataRender, setIndexDataRender] = useState(0);
   const [data, setData] = useState(0);
 
   const [rsGetData, setRsGetData] = useState("pending");
@@ -50,12 +55,15 @@ const BgTour = (props) => {
         if (res) {
           if (res.data.length > 0) {
             if (!isUnmounting) {
+              console.log("res=>>>> :", res);
               const filterData =
                 dateStart !== "" ? checkDateStart(res.data) : res.data;
               if (filterData && filterData.length > 0) {
                 console.log("filterData :", filterData);
                 setData(filterData);
                 setRsGetData("getFinish");
+                setIndexDataRender(0);
+                setCurrentPage(1);
               } else setRsGetData("null");
             }
           } else setRsGetData("null");
@@ -89,6 +97,37 @@ const BgTour = (props) => {
     return removeAccents(char);
   };
 
+  const nextPage = (number) => {
+    setIndexDataRender(number * _limit);
+    setCurrentPage(currentPage + 1);
+  };
+  const prePage = (number) => {
+    setIndexDataRender((number - 2) * _limit);
+    setCurrentPage(currentPage - 1);
+  };
+
+  const pagination = () => {
+    console.log("indexDataRender ", indexDataRender);
+    // const [indexDataRender, setindexDataRender] = useState(0);
+    // const [data, setData] = useState(0);
+    let datanew = [];
+    if (data.length <= 0) return;
+    let end =
+      indexDataRender + _limit >= data.length
+        ? data.length
+        : indexDataRender + _limit;
+
+    if (data.length === 1) return data;
+    console.log("indexDataRender :", indexDataRender);
+    console.log("end :", end);
+    for (let i = indexDataRender; i < end; i++) {
+      datanew.push(data[i]);
+    }
+    console.log("data ", data);
+    console.log("datanew ", datanew);
+    return datanew;
+  };
+
   return (
     <div className="container">
       <TourSearch
@@ -102,15 +141,25 @@ const BgTour = (props) => {
         <Waiting custome={{ position: "relative", top: "-90px" }} />
       )}
       {rsGetData === "getFinish" && (
-        <div
-          style={{
-            position: "relative",
-            top: "-113px",
-          }}
-          className="mover-list bg-light flex-wrap flex-grow p-3 rounded d-flex list-all-e-tour justify-content-center"
-        >
-          <BosxRsSearch data={data} />
-        </div>
+        <>
+          <div
+            style={{
+              position: "relative",
+              top: "-113px",
+            }}
+            className="mover-list bg-light flex-wrap flex-grow p-3 rounded d-flex list-all-e-tour justify-content-center"
+          >
+            {/* {console.log("pagination() :", )} */}
+            <BosxRsSearch data={pagination()} />
+          </div>
+          <MyPagination
+            nextPage={nextPage}
+            prePage={prePage}
+            data={data}
+            _limit={_limit}
+            currentPage={currentPage}
+          />
+        </>
       )}
       {rsGetData === "null" && (
         <NotFindData custome={{ position: "relative", top: "-90px" }} />

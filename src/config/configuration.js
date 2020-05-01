a7cba864650d16b8d25e3298578e8af129af6fb9
Anh <hotrunganhht1912@gmail.com>
2020-05-01 22:39:49 +0700

    fix

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const host = "http://localhost:4000";
-=======
 export const host = "http://localhost:3500";
->>>>>>> add History-booking
-=======
-export const host = "http://localhost:3500";
->>>>>>> e1a8351647fb7509f62ebf7a72b639afa961ae73

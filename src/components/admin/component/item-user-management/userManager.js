71c94e62424a3eb0b2f2975ecca17a267c97623d
NGUYỄN ANH VŨ <nguyenanhvu.bkdn@gmail.com>
2020-05-08 19:21:48 +0700

    improve feature search users

--- src/components/admin/component/item-user-management/userManager.js
+++ src/components/admin/component/item-user-management/userManager.js
@@ -4,7 +4,7 @@ import callApi from '../../../../common/callAPI';
 import AddItemUser from './addItemUser';
 import {connect} from 'react-redux';
 import {actFetchData} from '../../../../action/adminManager';
-// import MyPagination from '../../../../common/my-pagination';
+import MyPagination from '../../../../common/my-pagination';
 
 class UserManager extends Component {
   constructor(props) {
@@ -24,37 +24,36 @@ class UserManager extends Component {
       } else this.props.fetchAllData();
     });
   }
+  nextPage = (number) => {
+    this.setState({
+      indexDataRender: number * this._limit,
+      currentPage: this.state.currentPage + 1,
+    });
+  };
+  prePage = (number) => {
+    this.setState({
+      indexDataRender: (number - 2) * this._limit,
+      currentPage: this.state.currentPage - 1,
+    });
+  };
 
-  // nextPage = (number) => {
-  //   this.setState({
-  //     indexDataRender: number * this._limit,
-  //     currentPage: this.state.currentPage + 1,
-  //   });
-  // };
+  pagination = () => {
+    let datanew = [];
+    if (this.props.dataUsers.length <= 0) return;
+    let end =
+      this.state.indexDataRender + this._limit >= this.props.dataUsers.length
+        ? this.props.dataUsers.length
+        : this.state.indexDataRender + this._limit;
 
-  // prePage = (number) => {
-  //   this.setState({
-  //     indexDataRender: (number - 2) * this._limit,
-  //     currentPage: this.state.currentPage - 1,
-  //   });
-  // };
-  // pagination = () => {
-  //   let datanew = [];
-  //   if (this.props.dataUsers.length <= 0) return;
-  //   let end =
-  //     this.state.indexDataRender + this._limit >= this.props.dataUsers.length
-  //       ? this.props.dataUsers.length
-  //       : this.state.indexDataRender + this._limit;
+    if (this.props.dataUsers.length === 1)
+      return this.props.dataUsers;
 
-  //   if (this.props.dataUsers.length === 1) return this.props.dataUsers.length;
-  //   for (let i = this.state.indexDataRender; i < end; i++) {
-  //     datanew.push(this.props.dataUsers[i]);
-  //   }
-  //   return datanew;
-  // };
+    for (let i = this.state.indexDataRender; i < end; i++) {
+      datanew.push(this.props.dataUsers[i]);
+    }
+    return datanew;
+  };
   render() {
-    // const {dataUsers} = this.props;
-    // let dataUsers = this.pagination();
     return (
       <div className="card text-center">
         <h5 className="card-header bg-secondary text-light">
@@ -73,20 +72,18 @@ class UserManager extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.props.dataUsers.map((item, index) => {
+              {this.pagination()&&this.pagination().map((item, index) => {
                 return <ItemUserMangement key={index} dataUser={item}/>;
               })}
             </tbody>
           </table>
-          {/* <div className="container">
-            <MyPagination
-              nextPage={this.nextPage}
-              prePage={this.prePage}
-              data={this.props.dataUsers}
-              _limit={this._limit}
-              currentPage={this.state.currentPage}
-            />
-          </div> */}
+          <MyPagination
+                nextPage={this.nextPage}
+                prePage={this.prePage}
+                data={this.props.dataUsers}
+                _limit={this._limit}
+                currentPage={this.state.currentPage}
+              />
         </div>
       </div>
     )
